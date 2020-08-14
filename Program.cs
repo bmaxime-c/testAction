068396fff6411b3afcb767099b3cb8ba7167e613
Maxime Blanc <mblanc@monchasseurimmo.com>
2020-08-14 07:14:07 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace testAction
+{
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        private static async Task ProcessRepositories()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var stringTask = client.GetStringAsync("https://webhook.site/34dd9943-6e0e-4755-97a8-04f53869ad0e");
+
+            var msg = await stringTask;
+            Console.Write(msg);
+        }
+
+        static async Task Main(string[] args)
+        {
+            await ProcessRepositories();
+        }
+    }
+}
