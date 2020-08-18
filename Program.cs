0b73b7f18baae2184a3565712bf7faaee57f9b33
Maxime Blanc <mblanc@monchasseurimmo.com>
2020-08-19 01:06:43 +0200

    The test !

--- Program.cs
+++ Program.cs
@@ -16,37 +16,69 @@ namespace testAction
 {
     class Program
     {
-        private static readonly HttpClient client = new HttpClient();
+        /// <summary>
+        /// Extensions autorisées pour les fichiers de sous-titre
+        /// </summary>
+        private static readonly string[] SUBTITLE_EXTENSIONS = { ".sbv", ".srt", ".sub", ".mpsub", ".lrc", ".cap", ".smi", ".sami", ".rt", ".vtt", ".ttml", ".dfxp", ".scc", ".stl", ".tds", ".cin", ".asc" };
 
-        private static async Task ProcessRepositories(string[] files)
+        /// <summary>
+        /// Processes the list of files, checking wether the file exists, 
+        /// and if it is a subtitle, sends it to YouTube
+        /// </summary>
+        /// <param name="files">Array of files to process</param>
+        /// <returns></returns>
+        private static async Task ProcessModifiedFiles(string[] files)
         {
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(
-                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
-            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
-
-            string toPost = "";
-
-            foreach(var f in files)
+            foreach(FileInfo fi in files.Select(f => new FileInfo(f)))
             {
-                Console.WriteLine($"Reading file : {f}");
+                Console.WriteLine($"Processing file : {fi.Name}");
                 try
                 {
-                    var lines = File.ReadLines(f);
-                    toPost += lines.First() + Environment.NewLine;
+                    if(!fi.Exists)
+                    {
+                        Console.WriteLine($"File {fi.Name} does not exists, skip");
+                    }
+                    else if(!SUBTITLE_EXTENSIONS.Contains(fi.Extension))
+                    {
+                        Console.WriteLine($"File {fi.Name} is not recognized as a subtitle file, skip");
+                    }
+                    else
+                    {
+                        //It is a subtitle file, upload it to the video
+                        var dirParts = fi.Directory.Name.Split('.');
+                        var fileParts = fi.Name.Split('-');
+                        var language = fileParts[0].ToLower();
+                        var captionName = language;
+                        switch(language)
+                        {
+                            case "fr":
+                                captionName = "Français";
+                                break;
+
+                            case "en":
+                                captionName = "English";
+                                break;
+                        }
+
+                        await UploadVideoCaption(dirParts[0], language, captionName, fi);
+                    }
                 }
                 catch(Exception e)
                 {
-                    Console.WriteLine($"Error reading file {f} : {e}");
+                    Console.WriteLine($"Error reading file {fi.Name} : {e}");
                 }
             }
-
-            await client.PostAsync("https://webhook.site/34dd9943-6e0e-4755-97a8-04f53869ad0e", new StringContent(string.Join(',', files)));
-
-            Console.Write(string.Join(',', files));
         }
 
-        private async Task addVideoCaption(string videoID) //pass your video id here..
+        /// <summary>
+        /// Uploads given file as caption on YouTube video
+        /// </summary>
+        /// <param name="videoID">ID of the video where to import caption</param>
+        /// <param name="language">Language of the caption (ex: fr, en, ...)</param>
+        /// <param name="captionName">Name of the caption (should be human readable language name)</param>
+        /// <param name="fi">FileInfo pointing to caption file</param>
+        /// <returns></returns>
+        private static async Task UploadVideoCaption(string videoID, string language, string captionName, FileInfo fi)
         {
             UserCredential credential;
             //you should go out and get a json file that keeps your information... You can get that from the developers console...
@@ -56,23 +88,24 @@ namespace testAction
                     GoogleClientSecrets.Load(stream).Secrets,
                     new[] { YouTubeService.Scope.YoutubeForceSsl, YouTubeService.Scope.Youtube, YouTubeService.Scope.Youtubepartner },
                     "b.maximec@gmail.com",
-                    CancellationToken.None,
-                    new FileDataStore(GetType().ToString())
+                    CancellationToken.None
                 );
             }
             //creates the service...
             var youtubeService = new YouTubeService(new BaseClientService.Initializer()
             {
                 HttpClientInitializer = credential,
-                ApplicationName = GetType().ToString(),
+                ApplicationName = "UploadFromGithub",
             });
 
             //create a CaptionSnippet object...
-            CaptionSnippet capSnippet = new CaptionSnippet();
-            capSnippet.Language = "fr";
-            capSnippet.Name = videoID + "_Caption";
-            capSnippet.VideoId = videoID;
-            capSnippet.IsDraft = false;
+            CaptionSnippet capSnippet = new CaptionSnippet
+            {
+                Language = language.ToLower(),
+                Name = captionName,
+                VideoId = videoID,
+                IsDraft = false
+            };
 
             //create new caption object
             Caption caption = new Caption
@@ -81,26 +114,24 @@ namespace testAction
                 Snippet = capSnippet
             };
 
-            ////here we read our .srt which contains our subtitles/captions...
-            //using (var fileStream = new FileStream(TheFile.FilePath, FileMode.Open))
-            //{
-            //    //create the request now and insert our params...
-            //    var captionRequest = youtubeService.Captions.Insert(caption, "snippet", fileStream, "application/atom+xml");
+            //here we read our .srt which contains our subtitles/captions...
+            using (var fileStream = new FileStream(fi.FullName, FileMode.Open))
+            {
+                //create the request now and insert our params...
+                var captionRequest = youtubeService.Captions.Insert(caption, "snippet", fileStream, "application/atom+xml");
 
-            //    //finally upload the request... and wait.
-            //    await captionRequest.UploadAsync();
-            //}
+                //finally upload the request... and wait.
+                await captionRequest.UploadAsync();
+            }
         }
 
         static async Task Main(string[] args)
         {
+            //Reçoit en argument, le fichier contenant la liste des fichiers modifiés sur ce commit
             Console.WriteLine($"Received arg : {args[0]}");
-
-            Console.WriteLine("File exists ? " + File.Exists(args[0]));
-            Console.WriteLine("begin read file");
             var files = File.ReadAllText(args[0]);
             
-            await ProcessRepositories(files.Split(','));
+            await ProcessModifiedFiles(files.Split(','));
         }
     }
 }
